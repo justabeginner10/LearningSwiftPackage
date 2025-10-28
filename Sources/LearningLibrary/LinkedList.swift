bb770801218f002949a3e78e4a84a193d45c0f70
Aditya Raj <adityaraj60303@gmail.com>
2025-10-28 23:37:26 +0530

    ADD: Collection conformance to LinkedList

--- Sources/LearningLibrary/LinkedList.swift
+++ Sources/LearningLibrary/LinkedList.swift
@@ -139,3 +139,45 @@ extension LinkedList: CustomStringConvertible {
         return String(describing: head)
     }
 }
+
+extension LinkedList: Collection {
+    public struct Index: Comparable {
+        
+        public var node: Node<Value>?
+        
+        static public func ==(lhs: Index, rhs: Index) -> Bool {
+            switch(lhs.node, rhs.node) {
+            case let (left?, right?):
+                return left.next === right.next
+            case (nil, nil):
+                return true
+            default:
+                return false
+            }
+        }
+        
+        public static func < (lhs: LinkedList<Value>.Index, rhs: LinkedList<Value>.Index) -> Bool {
+            guard lhs != rhs else {
+                return false
+            }
+            let nodes = sequence(first: lhs.node) { $0?.next }
+            return nodes.contains { $0 === rhs.node }
+        }
+    }
+    
+    public var startIndex: Index {
+        return Index(node: head)
+    }
+    
+    public var endIndex: Index {
+        return Index(node: tail?.next)
+    }
+    
+    public func index(after i: Index) -> Index {
+        return Index(node: i.node?.next)
+    }
+    
+    public subscript(position: Index) -> Value {
+        position.node!.value
+    }
+}
